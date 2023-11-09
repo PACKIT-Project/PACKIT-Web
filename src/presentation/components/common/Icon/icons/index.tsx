990b866428692dc3874fe07508fc9f2f6d64cbbe
gabi <70315572+Limgabi@users.noreply.github.com>
2023-11-09 23:21:50 +0900

    design #92 : 온보딩 페이지 아이콘 추가

--- src/presentation/components/common/Icon/icons/index.tsx
+++ src/presentation/components/common/Icon/icons/index.tsx
@@ -1,57 +1,66 @@
-export { default as AuthLogo } from "./auth/AuthLogo";
-export { default as AuthKakao } from "./auth/AuthKakao";
-export { default as AuthNaver } from "./auth/AuthNaver";
-export { default as LoginComplete } from "./auth/LoginComplete";
+export { default as AuthLogo } from './auth/AuthLogo';
+export { default as AuthKakao } from './auth/AuthKakao';
+export { default as AuthNaver } from './auth/AuthNaver';
+export { default as LoginComplete } from './auth/LoginComplete';
 
-export { default as Linear1 } from "./loginComplate/Linear1";
-export { default as Linear2 } from "./loginComplate/Linear2";
+export { default as Time } from './onBoarding/Time';
+export { default as Vacation } from './onBoarding/Vacation';
+export { default as HandShake } from './onBoarding/HandShake';
+export { default as Memo } from './onBoarding/Memo';
+export { default as Sign } from './onBoarding/Sign';
+export { default as Search } from './onBoarding/Search';
+export { default as Idea } from './onBoarding/Idea';
+export { default as Notice } from './onBoarding/Notice';
 
-export { default as Arrow } from "./Arrow";
-export { default as Bus } from "./Bus";
-export { default as Plane } from "./Plane";
-export { default as Check } from "./Check";
-export { default as UnCheck } from "./UnCheck";
-export { default as InputChecked } from "./InputChecked";
-export { default as Checked } from "./Checked";
-export { default as Plus } from "./Plus";
+export { default as Linear1 } from './loginComplate/Linear1';
+export { default as Linear2 } from './loginComplate/Linear2';
 
-export { default as Calendar } from "./Calendar";
-export { default as MoreOutlined } from "./MoreOutlined";
-export { default as UpOutlined } from "./UpOutlined";
-export { default as DownOutlined } from "./DownOutlined";
-export { default as Delete } from "./Delete";
-export { default as SwapOutlined } from "./SwapOutlined";
-export { default as EllipsisOutlined } from "./EllipsisOutlined";
-export { default as KakaoLogo } from "./KakaoLogo";
-export { default as LinkOutlined } from "./LinkOutlined";
+export { default as Arrow } from './Arrow';
+export { default as Bus } from './Bus';
+export { default as Plane } from './Plane';
+export { default as Check } from './Check';
+export { default as UnCheck } from './UnCheck';
+export { default as InputChecked } from './InputChecked';
+export { default as Checked } from './Checked';
+export { default as Plus } from './Plus';
 
-export { default as Logo } from "./Logo";
-export { default as Folder } from "./Folder";
-export { default as User } from "./User";
+export { default as Calendar } from './Calendar';
+export { default as MoreOutlined } from './MoreOutlined';
+export { default as UpOutlined } from './UpOutlined';
+export { default as DownOutlined } from './DownOutlined';
+export { default as Delete } from './Delete';
+export { default as SwapOutlined } from './SwapOutlined';
+export { default as EllipsisOutlined } from './EllipsisOutlined';
+export { default as KakaoLogo } from './KakaoLogo';
+export { default as LinkOutlined } from './LinkOutlined';
 
-export { default as NotExistCheckList } from "./NotExistCheckList";
-export { default as TripCreateButton } from "./TripCreateButton";
-export { default as MenuXButton } from "./MenuXButton";
-export { default as Clip } from "./Clip";
-export { default as PlusCircle } from "./PlusCircle";
+export { default as Logo } from './Logo';
+export { default as Folder } from './Folder';
+export { default as User } from './User';
 
-export { default as Chevron } from "./Chevron";
-export { default as Complate } from "./Complate";
+export { default as NotExistCheckList } from './NotExistCheckList';
+export { default as TripCreateButton } from './TripCreateButton';
+export { default as MenuXButton } from './MenuXButton';
+export { default as Clip } from './Clip';
+export { default as PlusCircle } from './PlusCircle';
 
-export { default as Beach } from "./Beach";
-export { default as Family } from "./Family";
-export { default as Profile } from "./Profile";
+export { default as Chevron } from './Chevron';
+export { default as Complate } from './Complate';
 
-export { default as ModalXButton } from "./ModalXButton";
-export { default as EditProfile } from "./EditProfile";
-export { default as Template } from "./Template";
+export { default as Beach } from './Beach';
+export { default as Family } from './Family';
+export { default as Profile } from './Profile';
 
-export { CheckedBox, UnCheckedBox } from "./CheckBox";
-export { default as Remind } from "./Remind";
-export { default as CloseOutlined } from "./CloseOutlined";
-export { default as LoudSpeaker } from "./LoudSpeaker";
-export { FilledHeart, UnFilledHeart } from "./Heart";
-export { default as SliceOutlined } from "./SliceOutlined";
-export { default as CalendarGreen } from "./CalendarGreen";
-export { default as Email } from "./Email";
-export { default as DoubleCheckIcon } from "./DoubleCheckIcon";
+export { default as ModalXButton } from './ModalXButton';
+export { default as EditProfile } from './EditProfile';
+export { default as Template } from './Template';
+
+export { CheckedBox, UnCheckedBox } from './CheckBox';
+export { default as Remind } from './Remind';
+export { default as CloseOutlined } from './CloseOutlined';
+export { default as LoudSpeaker } from './LoudSpeaker';
+export { FilledHeart, UnFilledHeart } from './Heart';
+export { default as SliceOutlined } from './SliceOutlined';
+export { default as CalendarGreen } from './CalendarGreen';
+export { default as Email } from './Email';
+export { default as DoubleCheckIcon } from './DoubleCheckIcon';
---
+++ src/presentation/components/common/Icon/icons/onBoarding/Time.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Time = () => {
+  return (
+    <svg
+      width="51"
+      height="51"
+      viewBox="0 0 51 51"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M25.3252 46.0892C36.831 46.0892 46.1585 36.7617 46.1585 25.2558C46.1585 13.7499 36.831 4.42249 25.3252 4.42249C13.8192 4.42249 4.49182 13.7499 4.49182 25.2558C4.49182 36.7617 13.8192 46.0892 25.3252 46.0892Z"
+        fill="#71E582"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M22.7211 15.8809H27.9294V24.9954L35.2211 30.4642L32.0961 34.6309L22.7211 27.5996V15.8809Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
+
+export default Time;
