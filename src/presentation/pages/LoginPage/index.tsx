21441155c8e1711cb7c0ad8c94fef7e7e1f620cc
gabi <70315572+Limgabi@users.noreply.github.com>
2023-11-10 11:46:37 +0900

    design #96 : 로그인 페이지 퍼블리싱

--- src/presentation/pages/LoginPage/index.tsx
+++ src/presentation/pages/LoginPage/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { styled } from "styled-components";
-import Icon from "../../components/common/Icon";
-import COLOR from "@styles/colors";
-import { Link } from "react-router-dom";
-import Text from "@components/common/Text";
-import Spacing from "@components/common/Spacing";
+import React from 'react';
+import { styled } from 'styled-components';
+import Icon from '../../components/common/Icon';
+import COLOR from '@styles/colors';
+import { Link } from 'react-router-dom';
+import Text from '@components/common/Text';
+import Spacing from '@components/common/Spacing';
 
 const LoginPage = () => {
   const kakaoUrl = `${process.env.REACT_APP_BASE_URL}/oauth2/authorization/kakao?redirect_uri=${process.env.REACT_APP_REDIRECT_URL}`;
@@ -13,30 +13,26 @@ const LoginPage = () => {
   return (
     <LoginPageWrapper>
       <MainWrapper>
+        <Icon icon="AuthLogo" />
+        <Spacing size={17} />
         <Text
-          text="빈틈없는 여행의 시작"
+          text="빈틈없는 여행 준비의 시작"
           color={COLOR.MAIN_BLACK}
           fontSize={21}
           fontWeight={600}
           lineHeight="139%"
         />
-        <Spacing size={17} />
-        <Icon icon="AuthLogo" />
       </MainWrapper>
       <ButtonWrapper>
-        <Text
-          text="SNS 계정으로 로그인하기"
-          color={COLOR.GRAY_800}
-          fontSize={14}
-          fontWeight={600}
-          lineHeight="normal"
-        />
         <ButtonContainer>
           <Link to={kakaoUrl}>
             <Icon icon="AuthKakao" />
           </Link>
           <Link to={naverUrl}>
             <Icon icon="AuthNaver" />
+          </Link>
+          <Link to={naverUrl}>
+            <Icon icon="AuthApple" />
           </Link>
         </ButtonContainer>
       </ButtonWrapper>
@@ -49,11 +45,6 @@ const LoginPageWrapper = styled.div`
   width: 100%;
   height: 100%;
   overflow-y: hidden;
-  background: linear-gradient(
-    180deg,
-    rgba(67, 226, 168, 0.35) 0%,
-    rgba(253, 253, 253, 0) 100%
-  );
 `;
 
 const MainWrapper = styled.div`
@@ -65,7 +56,7 @@ const MainWrapper = styled.div`
 
 const ButtonWrapper = styled.div`
   position: fixed;
-  bottom: 6%;
+  bottom: 20%;
   left: 50%;
   transform: translateX(-50%);
 
