7dadffc9e6a6a90f73045808d93ea7ce15e2353e
gabi <70315572+Limgabi@users.noreply.github.com>
2023-11-15 08:12:59 +0900

    feat #101 : 여행 생성 바텀시트

---
+++ src/presentation/components/common/BottomNav/CreateTripModal.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import styled from 'styled-components';
+import Icon from '../Icon';
+import Text from '../Text';
+import COLOR from '@styles/colors';
+import { useNavigate } from 'react-router-dom';
+
+const CreateTripModal = () => {
+  const navigate = useNavigate();
+
+  return (
+    <CreateTripModalWrapper>
+      <ContentButton onClick={() => navigate('/trip-create/1')}>
+        <Icon icon="CreateTrip" cursor={true} />
+        <Text
+          text="새로운 여행 생성"
+          fontSize={17}
+          fontWeight={600}
+          lineHeight="18px"
+          color={COLOR.COOL_GRAY_200}
+        />
+      </ContentButton>
+      <hr />
+      <ContentButton>
+        <Icon icon="Key" cursor={true} />
+        <div className="text-wrapper">
+          <Text
+            text="초대코드로 참여"
+            fontSize={17}
+            fontWeight={600}
+            lineHeight="18px"
+            color={COLOR.COOL_GRAY_200}
+          />
+          <Text
+            text="공유받은 코드 입력하여 그룹 참여"
+            fontSize={13}
+            fontWeight={500}
+            lineHeight="18px"
+            color={COLOR.UI_GRAY_4}
+          />
+        </div>
+      </ContentButton>
+    </CreateTripModalWrapper>
+  );
+};
+
+export default CreateTripModal;
+
+const CreateTripModalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  width: 100%;
+  padding: 25px 0;
+
+  hr {
+    width: 100%;
+    height: 1px;
+    margin: 0;
+
+    border: 0;
+    background-color: #e9ecf0;
+  }
+`;
+
+const ContentButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  gap: 11px;
+  align-items: center;
+
+  outline: none;
+  border: none;
+  background-color: transparent;
+
+  .text-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 1px;
+    text-align: left;
+  }
+`;
--- src/presentation/components/common/BottomNav/index.tsx
+++ src/presentation/components/common/BottomNav/index.tsx
@@ -3,10 +3,14 @@ import COLOR from '@styles/colors';
 import styled from 'styled-components';
 import Icon from '../Icon';
 import { useLocation, useNavigate } from 'react-router-dom';
+import useModal from '@hooks/useModal';
+import BottomSheet from '../BottomSheet';
+import CreateTripModal from './CreateTripModal';
 
 const BottomNav = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
+  const { isShowModal, openModal, closeModal } = useModal();
 
   const routerObj = [
     { key: 'Home', value: '홈', clicked: pathname === '/', path: '/' },
@@ -29,11 +33,16 @@ const BottomNav = () => {
         </>
 
         <PlusButtonWrapper>
-          <PlusButton onClick={() => navigate('/trip-create/1')}>
+          <PlusButton onClick={openModal}>
             <Icon icon="Plus" width={36} height={36} fill="#FFF" cursor={true} />
           </PlusButton>
         </PlusButtonWrapper>
       </div>
+      {isShowModal && (
+        <BottomSheet isVisible={isShowModal} closeModal={closeModal}>
+          <CreateTripModal />
+        </BottomSheet>
+      )}
     </BottomNavWrapper>
   );
 };
