043232b9093e3cf2d9d606a7406ecebeea14f1d7
gabi <70315572+Limgabi@users.noreply.github.com>
2023-11-23 23:25:14 +0900

    fix #1 : 공통 레이아웃 간격 조정

--- src/presentation/components/common/AppLayout/index.tsx
+++ src/presentation/components/common/AppLayout/index.tsx
@@ -11,7 +11,7 @@ export default AppLayout;
 const Container = styled.div`
   height: 100%;
   width: 100%;
-  padding: 0 20px;
+  padding: 0 16px;
 
   box-sizing: border-box;
   background-color: ${COLOR.WHITE};
