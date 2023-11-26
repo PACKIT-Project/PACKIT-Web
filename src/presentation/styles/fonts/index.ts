3bc32e3824b4e7423a3a4685f11ccc6044142b66
gabi <70315572+Limgabi@users.noreply.github.com>
2023-11-26 22:27:26 +0900

    feat #4 : 타이포그래피 생성

---
+++ src/presentation/styles/fonts/index.ts
@@ -0,0 +1,78 @@
+export const TYPOGRAPHY: any = {
+  TITLE: {
+    DISPLAY1_BOLD: {
+      fontSize: 24,
+      fontWeight: 700,
+      lineHeight: '132%',
+      letterSpacing: '-0.5px',
+    },
+    DISPLAY2_SEMIBOLD: {
+      fontSize: 20,
+      fontWeight: 600,
+      lineHeight: '18px',
+      letterSpacing: '-0.5px',
+    },
+    SUBHEADING1_SEMIBOLD: {
+      fontSize: 18,
+      fontWeight: 600,
+      lineHeight: '18px',
+      letterSpacing: '-0.5px',
+    },
+    SUBHEADING2_BOLD: {
+      fontSize: 18,
+      fontWeight: 700,
+      lineHeight: '18px',
+    },
+    SUBHEADING4_MEDIUM: {
+      fontSize: 20,
+      fontWeight: 500,
+      lineHeight: '132%',
+      letterSpacing: '-0.5px',
+    },
+  },
+  TEXT: {
+    BODY1_MEDIUM: {
+      fontSize: 18,
+      fontWeight: 500,
+      lineHeight: '18px',
+      letterSpacing: '-0.5px',
+    },
+    BODY2_BOLD: {
+      fontSize: 16,
+      fontWeight: 700,
+      lineHeight: '20.5px',
+      letterSpacing: '-0.5px',
+    },
+    BODY3_SEMIBOLD: {
+      fontSize: 16,
+      fontWeight: 600,
+      lineHeight: '20.5px',
+      letterSpacing: '-0.5px',
+    },
+    BODY5_SEMIBOLD: {
+      fontSize: 17,
+      fontWeight: 600,
+      lineHeight: '18px',
+    },
+  },
+  DES: {
+    CAPTION1_SEMIBOLD: {
+      fontSize: 13,
+      fontWeight: 600,
+      lineHeight: '13px',
+    },
+    CAPTION2_MEDIUM: {
+      fontSize: 12,
+      fontWeight: 600,
+      lineHeight: '16px',
+      letterSpacing: '-0.12px',
+    },
+  },
+  BUTTON: {
+    LABEL1_BOLD: {
+      fontSize: 20,
+      fontWeight: 700,
+      lineHeight: 'normal',
+    },
+  },
+};
