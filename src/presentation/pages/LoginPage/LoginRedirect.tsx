6e5ad62c5f99e35882369c46d8dcbbe7b0691da4
gabi <70315572+Limgabi@users.noreply.github.com>
2023-12-03 14:40:22 +0900

    feat #6 : 신규 회원일 경우 온보딩 페이지로 라우팅

--- src/presentation/pages/LoginPage/LoginRedirect.tsx
+++ src/presentation/pages/LoginPage/LoginRedirect.tsx
@@ -1,22 +1,24 @@
-import React, { useEffect } from "react";
-import { setCookie } from "@utils/cookie";
-import { useLocation, useNavigate } from "react-router-dom";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
+import { setCookie } from '@utils/cookie';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const LoginRedirect = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const accessToken = searchParams.get("token") as string;
-  const memberState = searchParams.get("member-status") as string;
+  const accessToken = searchParams.get('access-token') as string;
+  const memberState = searchParams.get('member-status') as string;
 
   useEffect(() => {
-    setCookie("accessToken", accessToken, 1);
-    if (memberState === "NEW_MEMBER") {
-      navigate("/login/complate");
+    setCookie('accessToken', accessToken, 1);
+    if (memberState === 'WAITING_TO_JOIN') {
+      navigate('/onboarding');
     } else {
-      navigate("/");
+      navigate('/');
     }
   }, []);
+
   return <></>;
 };
 
