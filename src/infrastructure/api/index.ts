19803338d1b97df4bfd5d6f1150e6cbcbdf0b641
gabi <70315572+Limgabi@users.noreply.github.com>
2023-12-07 23:39:15 +0900

    fix #11 : 회원 에러코드 추가

--- src/infrastructure/api/index.ts
+++ src/infrastructure/api/index.ts
@@ -27,7 +27,8 @@ client.interceptors.response.use(
       response.data.errorCode === 'AT-C-0002' ||
       response.data.errorCode === 'AT-C-0001' ||
       response.data.errorCode === 'AT-C-0004' ||
-      response.data.errorCode === 'AT-C-0006'
+      response.data.errorCode === 'AT-C-0006' ||
+      response.data.errorCode === 'AT-C-9001'
     ) {
       const originalRequest = config;
 
