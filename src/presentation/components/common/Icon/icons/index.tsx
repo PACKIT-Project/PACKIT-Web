192441b0889744add9f4e16cccb7a1e2bc0b982b
gabi <70315572+Limgabi@users.noreply.github.com>
2023-12-10 15:00:28 +0900

    design #3 : 여행 초대

--- src/presentation/components/common/Icon/icons/index.tsx
+++ src/presentation/components/common/Icon/icons/index.tsx
@@ -79,3 +79,6 @@ export { default as KeyWhite } from './KeyWhite';
 export { default as Setting } from './Setting';
 export { default as ETC } from './ETC';
 export { default as LocationPin } from './LocationPin';
+
+export { default as InviteKakao } from './invite/InviteKakao';
+export { default as InviteCode } from './invite/InviteCode';
