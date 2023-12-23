67ee0b4c9878b02395b405a69bb6c7f413cffdf3
gabi <70315572+Limgabi@users.noreply.github.com>
2023-12-23 23:36:15 +0900

    feat #15 : 여행 조회 바텀 시트

--- src/presentation/components/MainPage/components/TripCard.tsx
+++ src/presentation/components/MainPage/components/TripCard.tsx
@@ -1,114 +1,57 @@
-import React from "react";
-import { styled } from "styled-components";
-import COLOR from "@styles/colors";
-import Spacing from "@components/common/Spacing";
-import Tag from "@components/common/Tag";
-import Text from "@components/common/Text";
-import { getTripDetailRange } from "../../../../application/utils/getDate";
-import ProgressBar from "@components/common/ProgressBar";
-import { useNavigate } from "react-router-dom";
-import HeartButton from "@components/common/HeartButton";
+import React from 'react';
+import { styled } from 'styled-components';
+import COLOR from '@styles/colors';
+import { useNavigate } from 'react-router-dom';
+import { TYPOGRAPHY } from '@styles/fonts';
+import Icon from '@components/common/Icon';
 
-const TripCard = ({ travelInfo }: any) => {
+const TripCard = ({ travel }: any) => {
   const navigate = useNavigate();
 
   const handleClickTravelDetail = () => {
-    navigate(`/trip/${travelInfo.travelId}`);
+    navigate(`/trip/${travel.id}`);
   };
 
   return (
-    <TripCardWrapper
-      progress={travelInfo.dDay.includes("-") ? true : false}
-      onClick={handleClickTravelDetail}
-    >
-      <TopWrapper>
-        <Tag
-          text={travelInfo.dDay}
-          backgroundColor={
-            travelInfo.dDay.includes("-") ? COLOR.GREEN_100 : COLOR.GRAY_200
-          }
-          color={travelInfo.dDay.includes("-") ? COLOR.MAIN_GREEN : "#5C5F64"}
-        />
-        <Tag
-          text={travelInfo.destinationType === "OVERSEAS" ? "해외" : "국내"}
-          backgroundColor={
-            travelInfo.destinationType === "OVERSEAS" ? "#D1DEFF" : "#FFE2D1"
-          }
-          color={travelInfo.destinationType === "OVERSEAS" ? "#5F8BFB" : "#FE984E"}
-        />
-      </TopWrapper>
-      <Spacing size={11} />
-      <MainWrapper>
-        <div className="trip-info">
-          <Text
-            text={travelInfo.title}
-            color={COLOR.GRAY_900}
-            fontSize={18}
-            fontWeight={600}
-            lineHeight="18px"
-          />
-          <Text
-            text={`${getTripDetailRange(
-              travelInfo.startDate
-            )} ~ ${getTripDetailRange(travelInfo.endDate)}`}
-            color={COLOR.GRAY_600}
-            fontSize={14}
-            fontWeight={500}
-            lineHeight="14px"
-          />
+    <TripCardWrapper onClick={handleClickTravelDetail}>
+      <div className="travelInfo">
+        <div className="travelName">{travel.title}</div>
+        <div className="travelDetail">
+          <Icon icon="LocationPin" />
+          {travel.destination} · {travel.startDate} -{' '}
+          {travel.endDate.split('.').slice(1, 3).join('.')}{' '}
+          <span className="dDay">&nbsp;&nbsp;{travel.dDay}일 남음</span>
         </div>
-        <HeartButton
-          isInStorage={travelInfo.isInStorage}
-          travelId={travelInfo.travelId}
-        />
-      </MainWrapper>
-      {travelInfo.dDay.includes("-") && (
-        <>
-          <Spacing size={15} />
-          <ProgressBar
-            max={String(travelInfo.finished + travelInfo.unfinished)}
-            value={String(travelInfo.finished)}
-            percent={false}
-            percentNumber={100}
-            size="small"
-            startColor="#06DCA8"
-            finishColor="#00B494"
-          />
-          <Spacing size={6} />
-          <Text
-            text={`잔여 리스트 총 ${travelInfo.unfinished}개`}
-            color={COLOR.GRAY_700}
-            fontSize={12}
-            fontWeight={600}
-            lineHeight="14px"
-          />
-        </>
-      )}
+      </div>
     </TripCardWrapper>
   );
 };
 
-const TripCardWrapper = styled.div<{ progress: boolean }>`
-  padding: ${({ progress }) => (progress ? "22px 19px 12px 19px" : "22px 19px")};
-  border: 2px solid ${COLOR.GRAY_100};
-  border-radius: 8px;
-  background: ${COLOR.WHITE};
-`;
-const TopWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 6px;
-`;
-const MainWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: flex-start;
+const TripCardWrapper = styled.div`
+  padding: 16px 0;
+  cursor: pointer;
 
-  .trip-info {
+  .travelInfo {
     display: flex;
     flex-direction: column;
-    gap: 6px;
+    gap: 4px;
+
+    .travelName {
+      ${TYPOGRAPHY.TEXT.BODY2_BOLD};
+      color: ${COLOR.COOL_GRAY_400};
+    }
+    .travelDetail {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      ${TYPOGRAPHY.DES.CAPTION3_SEMIBOLD};
+      color: ${COLOR.UI_GRAY_5};
+
+      .dDay {
+        color: ${COLOR.MAIN_BLUE};
+      }
+    }
   }
 `;
 
--- src/presentation/components/MainPage/components/TripList.tsx
+++ src/presentation/components/MainPage/components/TripList.tsx
@@ -1,111 +1,36 @@
-import React, { useEffect, useState } from "react";
-import Spacing from "@components/common/Spacing";
-import COLOR from "@styles/colors";
-import { styled } from "styled-components";
-import TripCard from "./TripCard";
-import { getUpcomingTravles } from "../../../../infrastructure/api/travel";
-import Text from "@components/common/Text";
-import Button from "@components/common/Button";
-import Icon from "@components/common/Icon";
-import useGetMyTravel from "../../../../application/hooks/queries/travel/useGetMyTravel";
-import { useNavigate, useLocation } from "react-router-dom";
+import React, { useState } from 'react';
+import Spacing from '@components/common/Spacing';
+import COLOR from '@styles/colors';
+import { styled } from 'styled-components';
+import TripCard from './TripCard';
+import useGetMyTravel from '../../../../application/hooks/queries/travel/useGetMyTravel';
+import { TYPOGRAPHY } from '@styles/fonts';
 
 const TripList = () => {
-  const navigate = useNavigate();
-  const { state: locationState } = useLocation();
-  const filterList = ["예정된 여행", "지난 여행"];
-  const [tripFilter, setTripFilter] = useState("예정된 여행");
-  const [recentTravel, setRecentTravel] = useState<any>();
+  const tripState = ['예정된 여행', '지난 여행'];
+  const [currentState, setCurrentState] = useState('예정된 여행');
+  const { data: travels } = useGetMyTravel(currentState);
 
-  const { data: travelData } = useGetMyTravel(tripFilter);
-
-  const handleClickFilter = async (option: string) => {
-    setTripFilter(option);
-  };
-
-  useEffect(() => {
-    const getRecentTravel = async () => {
-      const recent = await getUpcomingTravles();
-      setRecentTravel(recent.data[0]);
-    };
-    getRecentTravel();
-  }, []);
-
-  const convertRemindText = (title: string) => {
-    if (!title) return false;
-    const lastChar = title[title.length - 1];
-    const uni = lastChar.charCodeAt(0);
-
-    if (uni < 0xac00 || uni > 0xd7a3) return false;
-    return (uni - 0xac00) % 28 !== 0;
-  };
-  const subject = convertRemindText(recentTravel?.title) ? "이" : "가";
-  const dDayNumber = recentTravel?.dDay.split("D-")[1];
-
-  /* 리마인드페이지로 이동 */
-  useEffect(() => {
-    
-    if (localStorage.getItem("block_remind_page")==="true") {
-      if (dDayNumber && dDayNumber !== "1") localStorage.setItem("block_remind_page", "false");
-    } else {
-      if (dDayNumber === "1") navigate(`/remind/${recentTravel.travelId}`);
-    }
-  }, [recentTravel?.dDay]);
-  
-
-  const handleClickRemindButton = () => {
-    navigate(`/trip/${recentTravel.travelId}`);
-  };
   return (
     <>
-      {travelData && (
+      {travels && (
         <TripListWrapper>
-          {recentTravel && (
-            <Button
-              radius={8}
-              background="linear-gradient(159deg, #02C293 0%, #05D7A4 100%)"
-              padding="11px 12px"
-              border="none"
-              onClick={handleClickRemindButton}
-            >
-              <RemindButton>
-                <div className="remind">
-                  <Text
-                    text={`${recentTravel?.title}${subject} ${dDayNumber}일 남았어요`}
-                    color={COLOR.WHITE}
-                    fontSize={16}
-                    fontWeight={600}
-                    lineHeight="16px"
-                  />
-                </div>
-                <Icon icon="Chevron" color={COLOR.WHITE} fill={COLOR.WHITE} />
-              </RemindButton>
-            </Button>
-          )}
-
-          <Spacing size={28} />
-          <PeriodFilter>
-            {filterList.map((option) => (
-              <OptionPeriodFilter
-                key={option}
-                clicked={tripFilter === option}
-                onClick={() => handleClickFilter(option)}
+          <TopWrapper>
+            {tripState.map((state) => (
+              <StateText
+                key={state}
+                onClick={() => setCurrentState(state)}
+                selected={currentState === state}
               >
-                <Text
-                  text={option}
-                  fontSize={18}
-                  fontWeight={600}
-                  lineHeight="140%"
-                  color={tripFilter === option ? COLOR.GRAY_800 : COLOR.GRAY_500}
-                />
-              </OptionPeriodFilter>
+                {state}
+              </StateText>
             ))}
-          </PeriodFilter>
-          <Spacing size={18} />
+          </TopWrapper>
+          <Spacing size={19} />
           <TripListContainer>
-            {travelData &&
-              travelData.map((travel: any, index: number) => (
-                <TripCard key={index} travelInfo={travel} />
+            {travels &&
+              travels.map((travel: any) => (
+                <TripCard key={travel.id} travel={travel} />
               ))}
           </TripListContainer>
         </TripListWrapper>
@@ -114,36 +39,25 @@ const TripList = () => {
   );
 };
 
-const TripListWrapper = styled.div``;
-const RemindButton = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  .remind {
-    display: flex;
-    flex-direction: row;
-    gap: 5px;
-    align-items: center;
-  }
-`;
-const PeriodFilter = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 15px;
-
-  margin-left: -20px;
-  margin-right: -20px;
-  padding-left: 20px;
-  padding-right: 20px;
-`;
-const OptionPeriodFilter = styled.span<{ clicked: boolean }>`
-  text-decoration: ${({ clicked }) => (clicked ? "underline" : "none")};
-  text-decoration-color: ${COLOR.MAIN_GREEN};
-  text-decoration-thickness: 2px;
-  text-underline-offset: 8px;
+const TripListWrapper = styled.div`
+  width: 100%;
+  padding-top: 20px;
 `;
 
+const TopWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  justify-content: flex-start;
+`;
+
+const StateText = styled.div<{ selected: boolean }>`
+  ${TYPOGRAPHY.TITLE.SUBHEADING2_BOLD};
+  cursor: pointer;
+  color: ${({ selected }) => (selected ? COLOR.COOL_GRAY_400 : COLOR.UI_GRAY_3)};
+  border-bottom: ${({ selected }) =>
+    selected ? `2px solid ${COLOR.COOL_GRAY_400}` : 'none'};
+`;
 const TripListContainer = styled.div`
   display: flex;
   flex-direction: column;
