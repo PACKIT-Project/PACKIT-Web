66bad2753d96186daa62ae9f03a208680676f6a2
gabi <70315572+Limgabi@users.noreply.github.com>
2023-12-27 11:29:40 +0900

    feat #31 : 할 일 그룹 삭제

--- src/presentation/pages/ManageTodoPage/index.tsx
+++ src/presentation/pages/ManageTodoPage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import BackHeader from '@components/common/Header/BackHeader';
 import COLOR from '@styles/colors';
@@ -8,12 +8,39 @@ import { travelClusterListType } from '@type/cluster';
 import { travelCategoryListType } from '@type/category';
 import { TYPOGRAPHY } from '@styles/fonts';
 import Icon from '@components/common/Icon';
+import useModal from '@hooks/useModal';
+import ConfirmModal from '@components/common/Modal/ConfirmModal';
+import Modal from '@components/common/Modal';
+import { deleteCluster } from '@api/cluster';
+import Toast from '@components/common/Toast';
 
 const ManageTodoPage = () => {
+  const {
+    isShowModal: isShowDeleteToast,
+    openModal: openDeleteToast,
+    closeModal: closeDeleteToast,
+  } = useModal();
   const { pathname } = useLocation();
+  const {
+    isShowModal: isShowDeleteConfirmModal,
+    openModal: openDeleteConfirmModal,
+    closeModal: closeDeleteConfirmModal,
+  } = useModal();
+
   const travelId = Number(pathname.split('edit/')[1]);
 
-  const { data, isLoading } = useGetTravelTodoList(travelId);
+  const { data, isLoading, refetch } = useGetTravelTodoList(travelId);
+  const [currClusterId, setCurrClusterId] = useState(0);
+
+  const handleClickDeleteGroup = async () => {
+    const res = await deleteCluster(currClusterId);
+    if (res === '할 일 그룹이 삭제되었습니다.') {
+      closeDeleteConfirmModal();
+      refetch();
+      openDeleteToast();
+    }
+  };
+
   if (isLoading) {
     return <>로딩중</>;
   }
@@ -26,7 +53,14 @@ const ManageTodoPage = () => {
           <CategoryTodo key={cluster.clusterId}>
             <div className="cluster">
               <div className="title">{cluster.title}</div>
-              <button>삭제</button>
+              <button
+                onClick={() => {
+                  setCurrClusterId(cluster.clusterId);
+                  openDeleteConfirmModal();
+                }}
+              >
+                삭제
+              </button>
             </div>
             <div className="categories">
               {cluster.travelCategoryList?.map(
@@ -43,6 +77,23 @@ const ManageTodoPage = () => {
           </CategoryTodo>
         ))}
       </ClusterTodoWrapper>
+      {isShowDeleteConfirmModal && (
+        <Modal
+          isVisible={isShowDeleteConfirmModal}
+          closeModal={closeDeleteConfirmModal}
+        >
+          <ConfirmModal
+            title="준비물 그룹을 삭제하시겠어요?"
+            explainText="삭제하신 항목은 복구가 불가능합니다."
+            closeModal={closeDeleteConfirmModal}
+            onClick={handleClickDeleteGroup}
+            yesText="삭제"
+          />
+        </Modal>
+      )}
+      {isShowDeleteToast && (
+        <Toast close={closeDeleteToast}>삭제가 완료되었습니다.</Toast>
+      )}
     </ManageTodoWrapper>
   );
 };
